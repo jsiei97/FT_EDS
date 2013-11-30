b09b7e511721bee2bb9de842a49a785ec52fb37f
Johan Simonsson <github@fun-tech.se>
2013-11-30 17:44:09 +0100

    GPLv3

--- test/hw_FT_EDS/CmdParse.h
+++ test/hw_FT_EDS/CmdParse.h
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2013 Johan Simonsson
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef  __CMD_PARSE_H
 #define  __CMD_PARSE_H
 
@@ -37,9 +54,5 @@ class CmdParse
         void cmd_buff_print();
         void cmd_add_cmd(char* name, cmd_func cmd);
 };
-
-
-
-
 
 #endif  // __CMD_PARSE_H
--- test/test_FT_EDS/HexDump.h
+++ test/test_FT_EDS/HexDump.h
@@ -1,14 +1,37 @@
+/**
+ * @file HexDump.h
+ * @author Johan Simonsson
+ * @brief HexDump a QByteArray
+ */
+
+/*
+ * Copyright (C) 2013 Johan Simonsson
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef  __HEXDUMP_H
 #define  __HEXDUMP_H
 
 #define HEXDUMP(ARG) HexDump::dump(ARG, __func__, __LINE__)
 
-class HexDump 
+class HexDump
 {
-    private: 
-    public: 
+    private:
+    public:
         static void dump(QByteArray* raw, const char* func, int line);
 
 };
 
-#endif  // __HEXDUMP_H 
+#endif  // __HEXDUMP_H
